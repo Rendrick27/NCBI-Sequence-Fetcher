44f61e53b921777bee0f2380a0a77c6517a2d91d
Rendrick Carreira <rendrickcarreira.social@gmail.com>
2023-03-16 22:37:56 +0000

    as

--- Main.py
+++ Main.py
@@ -1,33 +1,42 @@
 #!/usr/bin/env python3
-
-import sys
+# Importar o módulo Bio.Entrez
 from Bio import Entrez
 
-# Parse command line arguments
-if len(sys.argv) != 3:
-    print("Usage: python3 retrieve_sequences.py <database> <search_term>")
-    sys.exit(1)
-
-database = sys.argv[1]
-search_term = sys.argv[2]
-
-# Set email address (required by Entrez)
+# Definir o email e a ferramenta para a API Entrez
 Entrez.email = "rendrickcarreira.social@gmail.com"
+Entrez.tool = "meu_script"
 
-# Use Entrez API to search for sequences
-handle = Entrez.esearch(db=database, term=search_term)
+# Receber os argumentos da linha de comando
+import sys
+database = sys.argv[1] # escolher a base de dados
+term = sys.argv[2] # passar o termo de pesquisa
+
+# Fazer uma consulta usando a função esearch com o recurso history
+handle = Entrez.esearch(db=database, term=term, usehistory="y")
 record = Entrez.read(handle)
 handle.close()
 
-# Retrieve sequences from search results
+# Obter os identificadores das sequências encontradas
 webenv = record["WebEnv"]
 query_key = record["QueryKey"]
-handle = Entrez.efetch(db=database, rettype="fasta", retmode="text", webenv=webenv, query_key=query_key, usehistory="y")
-sequences = handle.read()
-handle.close()
+count = int(record["Count"])
 
-# Print sequences to stdout
-print(sequences)
+# Fazer um pedido usando a função efetch para obter as sequências em formato FASTA
+batch_size = 10 # definir o tamanho do lote
+for start in range(0, count, batch_size):
+    end = min(count, start+batch_size)
+    print("A obter as sequências %i até %i" % (start+1, end))
+    fetch_handle = Entrez.efetch(db=database,
+                                 rettype="fasta",
+                                 retmode="text",
+                                 retstart=start,
+                                 retmax=batch_size,
+                                 webenv=webenv,
+                                 query_key=query_key)
+    data = fetch_handle.read()
+    fetch_handle.close()
+    # Escrever os dados para STDOUT
+    sys.stdout.write(data)
 
 ##Link: https://stuntspt.gitlab.io/asb2023/classes/class_05/index.html#/
 ##Criado por
