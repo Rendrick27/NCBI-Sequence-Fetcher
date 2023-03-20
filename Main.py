3a78bd815dc0c492584c8a22d803fbf57f9d92a4
Rendrick Carreira <rendrickcarreira.social@gmail.com>
2023-03-20 09:06:45 +0000

    Last minute changes

--- Main.py
+++ Main.py
@@ -1,43 +1,51 @@
-#Linux shebang line
+# Linux shebang line
 #!/usr/bin/env python3
 
-from Bio import Entrez # Import the Entrez module for accessing the NCBI databases
-import sys # Import the sys module for handling command-line arguments
+# Importa a biblioteca Bio.Entrez para acessar bancos de dados do NCBI
+from Bio import Entrez
 
+# Importa o módulo sys para lidar com argumentos de linha de comando
+import sys
+
+# Define a função fetch_sequences que busca sequências em um banco de dados
 def fetch_sequences(database, term):
 
-    # Email para Entrez API
-    Entrez.email = "rendrickcarreira.social@gmail.com" # Replace with your email address
+    # Definir um email para a API do Entrez
+    Entrez.email = "fakeemail@exemplo.com"
+
+    # Definir o nome da ferramenta que está sendo usada para acessar a API do Entrez
     Entrez.tool = "fetch_sequences.py"
 
-    # Use esearch to search the database and save the results to a history
+    # Usa o método esearch para pesquisar o banco de dados com o termo de busca e salva os resultados em um histórico
     search_results = Entrez.esearch(db=database, term=term, usehistory="y")
     search_results = Entrez.read(search_results)
-    webenv = search_results["WebEnv"] # Save the WebEnv and QueryKey for use in later API calls
+
+    # Salva o WebEnv e QueryKey para uso em chamadas API posteriores
+    webenv = search_results["WebEnv"]
     query_key = search_results["QueryKey"]
 
-    # Use efetch to fetch the results in batches and write to STDOUT
-    batch_size = 10 # Number of records to fetch at a time
+    # Usa o método efetch para buscar as sequências em lotes e escrevê-las no console
+    batch_size = 10 # Número de registros para buscar de uma vez
     for start in range(0, int(search_results["Count"]), batch_size):
-        end = min(int(search_results["Count"]), start+batch_size) # Calculate the end of the current batch
+        end = min(int(search_results["Count"]), start+batch_size) # Calcula o final do lote atual
         fetch_results = Entrez.efetch(db=database, rettype="fasta", retmode="text",
                                       retstart=start, retmax=batch_size, webenv=webenv, query_key=query_key)
-        sys.stdout.write(fetch_results.read()) # Write the results to STDOUT
-        fetch_results.close() # Close the handle to free up resources
+        sys.stdout.write(fetch_results.read()) # Escreve os resultados no console
+        fetch_results.close() # Fecha a conexão para liberar recursos
 
+# Verifica se o script está sendo executado como o programa principal
 if __name__ == '__main__':
-    # Get the command-line arguments
-    if len(sys.argv) != 3: # Check that there are exactly 2 arguments (the script name and the search term)
-        print(__doc__) # If not, print the usage information and exit with an error code
+
+    # Verifica se o número de argumentos está correto
+    if len(sys.argv) != 3:
+        # Se o número de argumentos estiver incorreto, imprime informações de uso e sai com um código de erro
+        print("Uso: python fetch_sequences.py [nome do banco de dados] [termo de busca]")
         sys.exit(1)
-    database = sys.argv[1] # Get the name of the database from the command line
-    term = sys.argv[2] # Get the search term from the command line
 
-    fetch_sequences(database, term) # Call the fetch_sequences function with the database and search term
+    # Obtém o nome do banco de dados e o termo de busca a partir dos argumentos da linha de comando
+    database = sys.argv[1]
+    term = sys.argv[2]
 
-##Link: https://stuntspt.gitlab.io/asb2023/classes/class_05/index.html#/
-##Criado por
-#David
-#Helder
-#Joana
-#Rendrick
+    # Chama a função fetch_sequences com os argumentos fornecidos pelo usuário
+    fetch_sequences(database, term)
+
--- ReadME.md
+++ ReadME.md
@@ -1,9 +1,36 @@
-## Download Python PiP
-Linux -> sudo apt-get install python-pip
+# NCBI Sequence Fetcher
+This is a Python script that fetches sequences from a database using the Bio.Entrez library.
 
-## Instalar Biopython 
+This work was proposed in the Curricular Unit of Biology Analysis and Sequences of the bioinformatics course https://stuntspt.gitlab.io/asb2023/classes/class_05/index.html#/
+
+
+## Installation
+This script requires **Python 3** and the **Bio.Entrez** library.
+
+### Python PiP
+sudo apt-get install python-pip
+
+### Biopython 
 pip install biopython
 
-## Exemplo de uso
-Abrir terminal -> python3 Main.py nucleotide "bos taurus[organism] AND CYTB[gene]"
+## Usage
+python fetch_sequences.py [database name] [search term]
 
+Where [database name] is the name of the database you want to search (e.g., "nucleotide" for the NCBI nucleotide database), and [search term] is the term you want to search for.
+
+### Examples
+
+To search the NCBI nucleotide database for sequences related to "E. coli", run:
+
+python fetch_sequences.py nucleotide "Escherichia coli"
+
+This will fetch the sequences in batches and print them to the console.
+
+## Credits
+This script uses the Bio.Entrez library from the Biopython project.
+
+## Created by
+David Cabrita 202100320
+Helder Marques 202100959
+Joana Fonseca 202100981
+Rendrick Carreira 201901365
